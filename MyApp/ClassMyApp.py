7a172d8985f3e538ee682d8120a5be85641f19d7
Miguel Oliveira <m.riem.oliveira@gmail.com>
2022-03-23 14:34:00 +0000

    Close #8. Implemmented.

---
+++ MyApp/ClassMyApp.py
@@ -0,0 +1,32 @@
+import pathlib
+import tkinter as tk
+import tkinter.ttk as ttk
+import pygubu
+
+PROJECT_PATH = './'
+PROJECT_UI = './MyApp.ui'
+
+
+class ClassMyApp:
+
+    def __init__(self, master=None):
+        self.builder = builder = pygubu.Builder()
+        builder.add_resource_path(PROJECT_PATH)
+        builder.add_from_file(PROJECT_UI)
+
+        # create main window
+        self.main_window = builder.get_object('toplevelMain', master)
+
+
+        builder.connect_callbacks(self)
+
+    def callbackCommandExit(self):
+        print('Bye bye!')
+        exit(0)
+
+    def run(self):
+        self.main_window.mainloop()
+
+
+
+
